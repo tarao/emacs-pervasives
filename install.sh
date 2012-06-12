6a27b4b9d50be62a55b89b5a8043ec5a7db7c151
tarao <tarao.gnn@gmail.com>
2012-06-12 12:02:18 +0000

    Rewrite.

--- install.sh
+++ install.sh
@@ -1,58 +1,182 @@
 #! /bin/sh
 
-INIT=~/.emacs.d/init.el
+SIGNATURE="emacs-pervasives"
 
+# remote info
 SRC_REPOS=tarao/emacs-pervasives
 GITHUB=http://github.com
 RAW_GITHUB=https://raw.github.com
 SRC=$GITHUB/$SRC_REPOS
 RAW_SRC=$RAW_GITHUB/$SRC_REPOS/master
-EMACSD=~/.emacs.d
-DST=$EMACSD/site-lisp
-SIGNATURE="emacs-pervasives"
 
-VCS=""
-REPOS_DIR=""
+# local info
+EMACSD="$HOME/.emacs.d"
+INIT_EL="$HOME/.emacs $HOME/.emacs.el $HOME/.emacs.d/init.el"
+PACKAGES="m2ym/popup-el m2ym/auto-complete emacsmirror/undo-tree"
+PACKAGE_DST=$EMACSD/site-lisp
 
-(which svn >/dev/null && [ -x `which svn` ]) && {
-    VCS="svn checkout"
-    REPOS_DIR="/trunk"
-}
-(which git >/dev/null && [ -x `which git` ]) && {
-    VCS="git clone"
-    REPOS_DIR=""
+testing() {
+    [ -n "$TEST" ] && return 0
+    return 1
 }
 
-[ "x$VCS" = 'x' ] && {
-    echo "You need to have git or svn installed."
-    exit
+silent() {
+    cmd="$1"; shift
+    if [ -n "$VERBOSE" ]; then
+        $cmd "$@" && return
+    else
+        $cmd "$@" >/dev/null 2>&1 && return
+    fi
+    return 1
 }
 
-mkdir -p $EMACSD
+executable() {
+    type "$1" >/dev/null && return 0
+    return 1
+}
 
-for f in ~/.emacs ~/.emacs.el ~/.emacs.d/init.el; do
-    [ -w "$f" ] && {
-        INIT=$f
-        break
+if executable wget; then
+    wcat() {
+        wget -O - "$1" 2>/dev/null && return
+        return 1
     }
-done
+elif executable curl; then
+    wcat() {
+        curl -L "$1" && return
+        return 1
+    }
+fi
 
-echo "Emacs init file: $INIT"
-grep $SIGNATURE $INIT >/dev/null 2>&1 || {
-    wget -O - $RAW_SRC/init.el >> $INIT
+is_github() {
+    pattern='^\(git\|https\?\)://github\.com/[a-zA-Z0-9_]\+/[^/]\+\(\.git\)\?$'
+    echo "$1" | grep "$pattern" >/dev/null && return
+    return 1
 }
 
-mkdir -p $DST
-cd $DST
+github_repos() {
+    pat_github='\(git\|https\?\):\/\/github\.com'
+    pat_repos='\([a-zA-Z0-9_]\+\/[^/]\+\?\)'
+    strip_ext='s/^\(.*\)\.git$/\1/'
+    echo "$1" | sed -e "s/^$pat_github\/$pat_repos$/\\2/" | sed -e "$strip_ext"
+}
 
-LOAD="-L ."
-for repos in m2ym/popup-el m2ym/auto-complete emacsmirror/undo-tree; do
-    [ -d "$repos" ] || $VCS "$GITHUB/$repos$REPOS_DIR" "$repos"
-    last=`pwd`
-    cd "$repos"
-    for f in `ls *.el`; do
-        emacs $LOAD --batch -f batch-byte-compile "$f"
+pgit_clone() {
+    url="$1"; shift
+
+    executable git && git clone "$url" "$@" && return
+    is_github "$url" || return 1
+    executable seeed || {
+        echo 'You need sed to retrieve repository without git command' >&2
+        return 1
+    }
+
+    http_repos=http://github.com/`github_repos "$url"`
+    https_repos=https://github.com/`github_repos "$url"`
+    svn_repos="$https_repos"
+
+    executable svn && svn checkout "$svn_repos/trunk" "$@" && return
+
+    executable tar && {
+        tar_url="$http_repos/tarball/master"
+        dir="$1"
+        mkdir -p "$dir"
+        wcat "$tar_url" | tar xfz - --strip-components 1 -C "$dir" && return
+    }
+
+    echo 'You need svn or tar to retrieve repository without git command' >&2
+    return 1
+}
+
+pgit() {
+    cmd="$1"; shift
+    case "$cmd" in
+    '' | '-h' | '--help')
+        echo 'pgit COMMAND'
+        ;;
+    *)
+       cmd="pgit_$cmd"
+       executable "$cmd" && $cmd "$@" && return
+       return 1
+       ;;
+    esac
+}
+
+clone() {
+    echo "Retrieving $GITHUB/$1"
+    testing && return
+
+    silent pgit clone "$GITHUB/$1" "$1" || {
+        echo 'You need either git, svn or tar to retrieve remote packages.'
+        return 1
+    }
+}
+
+select_init_el() {
+    for f in $@; do
+        [ -w "$f" ] && {
+            echo $f
+            return
+        }
     done
+    echo $f
+}
+
+install_init_el() {
+    init=`select_init_el $INIT_EL`
+    echo "Emacs init file: $init"
+
+    if grep "$SIGNATURE" "$init" >/dev/null 2>&1; then
+        echo "$SIGNATURE is already installed"
+        return
+    fi
+
+    echo "Adding $RAW_SRC/init.el to $init"
+    testing || wcat "$RAW_SRC/init.el" >> "$init"
+}
+
+emacs_compile() {
+    echo -n "Compiling '$2'..."
+    testing || {
+        if silent emacs $1 --batch -f batch-byte-compile "$2"; then
+            echo 'done'
+        else
+            echo 'fail'
+        fi
+    }
+}
+
+compile_els() {
+    [ -d "$2" ] || return
+
+    last=`pwd`
+    cd "$2"
+
+    for f in `ls *.el`; do
+        emacs_compile "$1" "$f"
+    done
+
     cd "$last"
-    LOAD="$LOAD -L $last/$repos"
-done
+}
+
+install_packages() {
+    load_path='-L .'
+    for repos in "$@"; do
+        [ -d "$repos" ] || clone "$repos"
+        dir=`pwd`
+        compile_els "$load_path" "$repos"
+        load_path="$load_path -L $dir/$repos"
+    done
+}
+
+( executable grep && executable wcat ) || {
+    echo 'You need the following commands:'
+    echo '  - grep'
+    echo '  - wget or curl'
+    exit 1
+}
+
+mkdir -p "$EMACSD"
+install_init_el
+mkdir -p "$PACKAGE_DST"
+cd "$PACKAGE_DST"
+install_packages $PACKAGES
